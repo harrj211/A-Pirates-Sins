e327325c0179a9328ab3dc9832b99b62d58ea6c6
Joshua Harrison <harrj211@hsd.k12.or.us>
2023-12-06 13:56:58 -0800

    added some sounds

--- Assets/scripts/LaunchProjectile.cs
+++ Assets/scripts/LaunchProjectile.cs
@@ -8,19 +8,28 @@ public class LaunchProjectile : MonoBehaviour
     public float launchVelocity = 700f;
     public ParticleSystem muzzleflash;
     float m_fieldofview;
+    public AudioSource audioSource;
+    public AudioClip clip;
+    public float volume;
+
 
     Vector3 launcher;
 
     void Update()
     {
         m_fieldofview = 60f;
+        
+        
 
         if (Input.GetButtonDown("Fire1"))
         {
+            audioSource.PlayOneShot(clip, volume);
+
             GameObject ball = Instantiate(projectile, transform.position, transform.rotation);
             ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, 0, launchVelocity));
 
             muzzleflash.Play();
+
         }
 
         else
