03268e5a71beb612c194e30c1287493c5c6a77e0
Joshua Harrison <harrj211@hsd.k12.or.us>
2023-12-08 14:18:50 -0800

    Ugh

--- Assets/scripts/LaunchProjectile.cs
+++ Assets/scripts/LaunchProjectile.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class LaunchProjectile : MonoBehaviour
 {
+    //Projectile Launching
     public GameObject projectile;
     public float launchVelocity = 700f;
     public ParticleSystem muzzleflash;
+    //Field of view
     float m_fieldofview;
+    //Audio (Song)
     public AudioSource audioSource;
     public AudioClip clip;
     public float volume;
+    //Cooldown timer
+    public float buttonDelay = 3.0f; //the delay between button presses
+    float lastButtonTime = 0; //cache the last pressed time
 
 
     Vector3 launcher;
@@ -19,15 +25,18 @@ public class LaunchProjectile : MonoBehaviour
     {
         m_fieldofview = 60f;
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButton("Fire1") && Time.time >= lastButtonTime)
         {
+         
+           lastButtonTime = Time.time + buttonDelay;
+
             audioSource.PlayOneShot(clip, volume);
 
             GameObject ball = Instantiate(projectile, transform.position, transform.rotation);
             ball.GetComponent<Rigidbody>().AddRelativeForce(new Vector3 (0, 0, launchVelocity));
 
-            muzzleflash.Play();
-
+             muzzleflash.Play();
+          
         }
 
         else
