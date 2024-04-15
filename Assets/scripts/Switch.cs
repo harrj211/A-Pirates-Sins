2873b8499ee9df2b6751a8b040a4fe892231d956
Joshua Harrison <harrj211@hsd.k12.or.us>
2024-04-15 13:51:51 -0700

    Creted the title screen switching script

--- Assets/scripts/Switch.cs
+++ Assets/scripts/Switch.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Switch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    int title = 1;
     // Update is called once per frame
     void Update()
     {
-        
+        if(title == 1 && Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene (sceneName:"Prison");
+            title = 0;
+        }
     }
+
 }
